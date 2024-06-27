c248d95cb5eeeb388ddf212c1be06220dd0a1fde
José Dyego Henrique Cardoso de Sousa <116648398+dyegohrq@users.noreply.github.com>
2024-06-27 19:38:44 -0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const burger = document.getElementById('burger')
 const headerContainer = document.getElementById('header_container')
 const navMenuMobile = document.getElementById('nav_menu_mobile')
+const list = document.getElementById('list')
+const li = list.querySelectorAll('#list li')
 
 navMenuMobile.addEventListener('click', animarMenu)
 
@@ -8,3 +10,16 @@ function animarMenu() {
     headerContainer.classList.toggle('active')
     burger.classList.toggle('open')
 }
+
+function selectItem() {
+    li.forEach((li) => {
+        li.classList.remove('active_li')
+    })
+
+    this.classList.add('active_li')
+}
+
+
+li.forEach((li) => {
+    li.addEventListener('click', selectItem)
+})
